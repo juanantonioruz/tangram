8d790b28f6a45ed9bd2ff17e482bb6ebc6a57b14
juanantonioruz <juanantonioruz@gmail.com>
2013-04-01 10:30:09 +0200

    sending mail ok

--- src/clj/elavio/mailing.clj
+++ src/clj/elavio/mailing.clj
@@ -1,5 +1,20 @@
 (ns elavio.mailing
+  (:use [postal.core :as postal])
 
+  )
+
+(defn mailto [target subject body]
+  (try
+    (postal/send-message ^{:host "smtp.sendgrid.net"
+                         :user (System/getenv "SENDGRID_USERNAME")
+                         :pass (System/getenv "SENDGRID_PASSWORD")}
+                       {:from "group@professor-sori.com"
+                        :to target
+                        :subject subject
+                        :body body})
+    (catch Exception e (println (.getMessage e)) {:error true})
+    (finally {:error true})
+    )
   )
 
 
